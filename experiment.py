ff426dbaca810fe5b80dfb013457275c23c084e4
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-07-20 09:48:51 -0700

    initial clone of berry pilot

---
+++ experiment.py
@@ -0,0 +1,149 @@
+"""Pilot experiment for the berry game."""
+
+from wallace.experiments import Experiment
+from wallace.models import Info
+from wallace.networks import Empty
+from sqlalchemy import Integer, Float, Boolean
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.sql.expression import cast
+
+
+class BerryPilot(Experiment):
+    """Define the structure of the experiment."""
+
+    def __init__(self, session):
+        """Call the same function in the super (see experiments.py in wallace).
+
+        A few properties are then overwritten.
+        Finally, setup() is called.
+        """
+        super(BerryPilot, self).__init__(session)
+        self.experiment_repeats = 1
+        self.initial_recruitment_size = 3
+        self.known_classes["Decision"] = Decision
+        self.min_acceptable_performance = 1.00
+        self.num_trials = 120
+        self.setup()
+
+    def recruit(self):
+        """pass."""
+        pass
+
+    def create_network(self):
+        """Use the Empty network."""
+        return Empty()
+
+    def attention_check(self, participant):
+        """Check that the data are acceptable."""
+        infos = participant.infos()
+
+        score = (
+            float(len([i for i in infos if i.right is True])) /
+            (float(len(infos)))
+        )
+
+        return score >= self.min_acceptable_performance
+
+    def bonus(self, participant):
+        """The bonus to be awarded to the given participant."""
+        infos = participant.infos()
+
+        score = (
+            float(len([i for i in infos if i.right])) /
+            (float(len(infos)))
+        )
+
+        print "score: {}".format(score)
+
+        return round(min(max((score - 0.5) * 2, 0), 1), 2)
+
+
+class Decision(Info):
+    """A decision."""
+
+    __mapper_args__ = {"polymorphic_identity": "decision"}
+
+    """Property 1"""
+
+    @hybrid_property
+    def dimension(self):
+        """Convert property1 to dimension."""
+        return self.property1
+
+    @dimension.setter
+    def dimension(self, dimension):
+        """Make dimension settable."""
+        self.property1 = dimension
+
+    @dimension.expression
+    def dimension(self):
+        """Make dimension queryable."""
+        return self.property1
+
+    """Property 2"""
+
+    @hybrid_property
+    def trial(self):
+        """Convert property2 to trial."""
+        return int(self.property2)
+
+    @trial.setter
+    def trial(self, trial):
+        """Make trial settable."""
+        self.property2 = repr(trial)
+
+    @trial.expression
+    def trial(self):
+        """Make trial queryable."""
+        return cast(self.property2, Integer)
+
+    """Property 3"""
+
+    @hybrid_property
+    def value(self):
+        """Convert property3 to value."""
+        return float(self.property3)
+
+    @value.setter
+    def value(self, value):
+        """Make value settable."""
+        self.property3 = repr(value)
+
+    @value.expression
+    def value(self):
+        """Make value queryable."""
+        return cast(self.property3, Float)
+
+    """Property 4"""
+
+    @hybrid_property
+    def dimensions(self):
+        """Convert property4 to dimensions."""
+        return self.property4
+
+    @dimensions.setter
+    def dimensions(self, dimensions):
+        """Make dimensions settable."""
+        self.property4 = dimensions
+
+    @dimensions.expression
+    def dimensions(self):
+        """Make dimensions queryable."""
+        return self.property4
+
+    """Property 5"""
+
+    @hybrid_property
+    def right(self):
+        """Convert property5 to right."""
+        return self.property5 in ["true", "True"]
+
+    @right.setter
+    def right(self, right):
+        """Make right settable."""
+        self.property5 = repr(right)
+
+    @right.expression
+    def right(self):
+        """Make right queryable."""
+        return cast(self.property5, Boolean)
---
+++ static/scripts/experiment.js
@@ -0,0 +1,170 @@
+lock = true;
+current_trial = 0;
+dimensions = ["color", "shininess", "spottiness"];
+
+insert_berry_number = function() {
+    reqwest({
+        url: "/experiment_property/num_trials",
+        method: 'get',
+        type: 'json',
+        success: function (resp) {
+            $("#n_berries").html(resp.num_trials);
+        },
+    });
+};
+
+start_trials = function() {
+    reqwest({
+        url: "/experiment_property/num_trials",
+        method: 'get',
+        type: 'json',
+        success: function (resp) {
+            blah = resp;
+            num_berries = resp.num_trials;
+            colors = [];
+            shininesses = [];
+            spottinesses = [];
+            for (i = 0; i<num_berries; i++) {
+                colors.push((Math.random() * 0.5 + 0.25).toFixed(2));
+                shininesses.push((Math.random() * 0.5 + 0.25).toFixed(2));
+                spottinesses.push((Math.random() * 0.5 + 0.25).toFixed(2));
+            }
+            create_agent();
+        },
+    });
+};
+
+// Create the agent.
+create_agent = function() {
+    reqwest({
+        url: "/node/" + participant_id,
+        method: 'post',
+        type: 'json',
+        success: function (resp) {
+            my_node_id = resp.node.id;
+            present_stimulus();
+        },
+        error: function (err) {
+            console.log(err);
+            err_response = JSON.parse(err.response);
+            if (err_response.hasOwnProperty('html')) {
+                $('body').html(err_response.html);
+            } else {
+                allow_exit();
+                go_to_page('postquestionnaire');
+            }
+        }
+    });
+};
+
+present_stimulus = function() {
+    $("#title").css('visibility', 'hidden');
+    $(".berry_div").css('visibility', 'hidden');
+    $("#button_div").css('visibility', 'hidden');
+
+    $("#trial_counter").html("This is trial " + (current_trial+1) + " of " + num_berries + ".");
+
+    color = colors[current_trial];
+    shininess = shininesses[current_trial];
+    spottiness = spottinesses[current_trial];
+
+    $("#red_body").css("opacity", color);
+    $("#glint").css("opacity", shininess);
+    $("#spots").css("opacity", spottiness);
+
+    dimension = dimensions[Math.floor(Math.random()*3)];
+    if (dimension == "color") {
+        value = colors[current_trial];
+        $("#title").html("Is this berry red or blue?");
+        $("#left_button").html("Red");
+        $("#right_button").html("Blue");
+    } else if (dimension == "shininess") {
+        value = shininesses[current_trial];
+        $("#title").html("Is this berry shiny or dull?");
+        $("#left_button").html("Shiny");
+        $("#right_button").html("Dull");
+    } else {
+        value = spottinesses[current_trial];
+        $("#title").html("Does this berry have bold spots or pale spots?");
+        $("#left_button").html("Bold");
+        $("#right_button").html("Pale");
+    }
+    setTimeout(show1, 500);
+    setTimeout(show1b, 1500);
+    setTimeout(show2, 2500);
+};
+
+show1b = function() {
+    $(".berry_div").css('visibility', 'visible');
+};
+
+show1 = function() {
+    $("#title").css('visibility', 'visible');
+};
+
+show2 = function() {
+    $("#button_div").css('visibility', 'visible');
+    lock = false;
+};
+
+$(document).keydown(function(e) {
+    var code = e.keyCode || e.which;
+    if (code == 37) {
+        left_click();
+    } else if (code == 39) {
+        right_click();
+    }
+});
+
+left_click = function() {
+    if (lock === false) {
+        right = value > 0.5;
+        lock = true;
+        if (dimension == "color") {
+            submit_response("red");
+        } else if (dimension == "shininess") {
+            submit_response("shiny");
+        } else {
+            submit_response("bold");
+        }
+    }
+};
+
+right_click = function() {
+    if (lock === false) {
+        right = value < 0.5;
+        lock = true;
+        if (dimension == "color") {
+            submit_response("blue");
+        } else if (dimension == "shininess") {
+            submit_response("dull");
+        } else {
+            submit_response("pale");
+        }
+    }
+};
+
+submit_response = function(response) {
+    reqwest({
+        url: "/info/" + my_node_id,
+        method: 'post',
+        data: {
+            contents: response,
+            info_type: "Decision",
+            property1: dimension,
+            property2: current_trial,
+            property3: value,
+            property4: [color, shininess, spottiness].toString(),
+            property5: right
+        },
+        success: function (resp) {
+            current_trial += 1;
+            if (current_trial<num_berries) {
+                present_stimulus();
+            } else {
+                allow_exit();
+                go_to_page('postquestionnaire');
+            }
+        }
+    });
+};
